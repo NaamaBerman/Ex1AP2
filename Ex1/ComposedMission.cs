7f2ff3533299f5dcc3368bcc87c2b7f5541c24af
Naama <naamaberman@gmail.com>
2019-03-23 20:59:22 +0200

    Added given files.

---
+++ Ex1/ComposedMission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class ComposedMission : IMission
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string Type => throw new NotImplementedException();
+
+        public event EventHandler<double> OnCalculate;
+
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
